767a4404de4dde19b17f6077a8942b4f832b0a46
hao.nguyen <Hao.NguyenHuu@Nashtechglobal.com>
2018-08-08 22:10:32 +0700

    build bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,11 @@ const gulp = require("gulp"),
 	del = require("del"),
 	connect = require("gulp-connect"),
 	autoprefixer = require("gulp-autoprefixer"),
-	sourcemaps = require("gulp-sourcemaps");
+	sourcemaps = require("gulp-sourcemaps"),
+	cleanCSS = require("gulp-clean-css"),
+	concat = require('gulp-concat');
 	
-var CONFIGS = {
+const CONFIGS = {
 	"src": "assets",
 	"srcJs": "assets/js",
 	"srcScss": "assets/scss",
@@ -17,7 +19,12 @@ var CONFIGS = {
 	"destCss": "public/css",
 	"destImg": "public/img",
 	"destImage": "public/images",
+	"destFont": "public/fonts",
 	"mapSrc": "map"
+};
+
+const IMPORT_CONFIGS = {
+	"fontAwesomeDir": "./node_modules/font-awesome-scss"
 };
 
 // create server
@@ -28,11 +35,6 @@ gulp.task('connect', function () {
 		port: 6996,
 		livereload: true
 	});
-});
-
-// clean css file before building
-gulp.task('clean-sass', function () {
-	return del([CONFIGS.destCss]);
 });
 
 // clean js file before building
@@ -49,16 +51,31 @@ gulp.task('clean-image', function () {
 	return del([CONFIGS.destImage]);
 });
 
+gulp.task('font-awesome-icons', function() {
+    return gulp.src(IMPORT_CONFIGS.fontAwesomeDir + '/fonts/*.{eot,svg,ttf,woff,woff2}')
+        .pipe(gulp.dest([CONFIGS.destFont]));
+});
+
 // build sass
-gulp.task('sass', ['clean-sass'], function () {
-	return gulp.src(CONFIGS.srcScss + '/**/*.scss')
+gulp.task('sass', function () {
+	return gulp.src([
+		'node_modules/bootstrap/dist/css/bootstrap.css',
+		CONFIGS.srcScss + '/**/*.scss'])
 		.pipe(sourcemaps.init())
-		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(cleanCSS())
+		.pipe(sass({
+			outputStyle: 'compressed',
+			loadPath: [
+				IMPORT_CONFIGS.bootstrapSrc + "/stylesheets",
+				IMPORT_CONFIGS.fontAwesomeSrc + "/scss"
+			]
+		}).on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions', 'ie 9'],
 			cascade: false
 		}))
 		.pipe(sourcemaps.write([CONFIGS.mapSrc]))
+		.pipe(concat('styles.css'))
 		.pipe(gulp.dest(CONFIGS.destCss))
 		.pipe(connect.reload());
 });
@@ -73,7 +90,7 @@ gulp.task('js', ['clean-js'], function () {
 });
 
 
-gulp.task('default', ['connect', 'sass', 'js'], function () {
+gulp.task('default', ['connect', 'sass', 'js', 'font-awesome-icons'], function () {
 	let watchScssSrc = CONFIGS.srcScss + "/**/*.scss";
 	let watchJsSrc = CONFIGS.srcJs + "/**/*.js";
 
