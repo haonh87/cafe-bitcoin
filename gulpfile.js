8af83cf998121109ff552548f3d8489ac1276fd2
hao.nguyen <haonguyenh2@HarveyNash.vn.local>
2018-08-09 21:32:33 +0700

    create index

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ const CONFIGS = {
 	"srcScss": "assets/scss",
 	"srcImg": "assets/img",
 	"srcImage": "assets/images",
+	"srcFont": "assets/fonts",
 	"dest": "public",
 	"destJs": "public/js",
 	"destCss": "public/css",
@@ -23,15 +24,16 @@ const CONFIGS = {
 	"mapSrc": "map"
 };
 
-const IMPORT_CONFIGS = {
-	"fontAwesomeDir": "./node_modules/font-awesome-scss"
+var IMPORT_CONFIGS = {
+	"bootstrapDir": "node_modules/bootstrap/dist",
+	"fontAwesomeDir": "node_modules/font-awesome-scss",
+	"jqueryDir": "node_modules/jquery/dist"
 };
 
 // create server
 gulp.task('connect', function () {
 	connect.server({
-		name: 'CAFE - BITCOIN',
-		root: '',
+		root: '.',
 		port: 6996,
 		livereload: true
 	});
@@ -51,29 +53,20 @@ gulp.task('clean-image', function () {
 	return del([CONFIGS.destImage]);
 });
 
-gulp.task('font-awesome-icons', function() {
-    return gulp.src(IMPORT_CONFIGS.fontAwesomeDir + '/fonts/*.{eot,svg,ttf,woff,woff2}')
-        .pipe(gulp.dest([CONFIGS.destFont]));
-});
-
 // build sass
 gulp.task('sass', function () {
 	return gulp.src([
-		'node_modules/bootstrap/dist/css/bootstrap.css',
+		IMPORT_CONFIGS.bootstrapDir + "/css/bootstrap.css",
 		CONFIGS.srcScss + '/**/*.scss'])
 		.pipe(sourcemaps.init())
-		.pipe(cleanCSS())
 		.pipe(sass({
 			outputStyle: 'compressed',
-			loadPath: [
-				IMPORT_CONFIGS.bootstrapSrc + "/stylesheets",
-				IMPORT_CONFIGS.fontAwesomeSrc + "/scss"
-			]
 		}).on('error', sass.logError))
 		.pipe(autoprefixer({
 			browsers: ['last 2 versions', 'ie 9'],
 			cascade: false
 		}))
+		.pipe(cleanCSS())
 		.pipe(sourcemaps.write([CONFIGS.mapSrc]))
 		.pipe(concat('styles.css'))
 		.pipe(gulp.dest(CONFIGS.destCss))
@@ -89,11 +82,42 @@ gulp.task('js', ['clean-js'], function () {
 		.pipe(connect.reload());
 });
 
+gulp.task('copy-img', function () {
+	return gulp.src([CONFIGS.srcImg + "/*.*"])
+		.pipe(gulp.dest(CONFIGS.destImg));
+});
 
-gulp.task('default', ['connect', 'sass', 'js', 'font-awesome-icons'], function () {
+gulp.task('copy-jquery', function () {
+	return gulp.src([
+		IMPORT_CONFIGS.jqueryDir + "/jquery.min.js",
+		IMPORT_CONFIGS.bootstrapDir + "/js/bootstrap.min.js"
+	])
+		.pipe(gulp.dest(CONFIGS.destJs));
+});
+
+gulp.task('copy-bootstrap-jquery', function () {
+	return gulp.src([
+		IMPORT_CONFIGS.bootstrapDir + "/js/bootstrap.min.js"
+	])
+		.pipe(gulp.dest(CONFIGS.destJs));
+});
+
+gulp.task('copy-fonts', function () {
+	return gulp.src([IMPORT_CONFIGS.fontAwesomeDir + "/fonts/*.{eot,svg,ttf,woff,woff2}"])
+        .pipe(gulp.dest(CONFIGS.destFont));
+});
+
+gulp.task('html', function () {
+	return gulp.src(['./*.html'])
+		.pipe(connect.reload());
+});
+
+
+gulp.task('default', ['connect', 'sass', 'js', 'copy-fonts', 'copy-jquery', 'copy-bootstrap-jquery', 'copy-img'], function () {
 	let watchScssSrc = CONFIGS.srcScss + "/**/*.scss";
 	let watchJsSrc = CONFIGS.srcJs + "/**/*.js";
 
+	gulp.watch(['**/*.html'], ['html']);
 	gulp.watch(watchScssSrc, ['sass']);
 	gulp.watch(watchJsSrc, ['js']);
 });
